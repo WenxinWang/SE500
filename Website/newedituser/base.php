fe77fcba3c148dd655c6f7150cc918396daba49c
latilm <latilm@my.erau.edu>
2015-11-03 16:09:16 -0500

    Create User 3.0.1

--- Website/newedituser/base.php
+++ Website/newedituser/base.php
@@ -9,7 +9,7 @@ function createUser()
 {
 
 
-$con = mysqli_connect("localhost","spr_erau","$PRfall2015@ERAU", "SE500spr");
+$con = mysqli_connect("localhost","spr_erau","asdf", "SE500spr");
 
 if (!$con)
   {
