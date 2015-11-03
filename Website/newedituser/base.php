024a255f2fab581d7fe381c0df3d758e3532bd0f
latilm <latilm@my.erau.edu>
2015-11-03 14:53:43 -0500

    Create User 1.6

--- Website/newedituser/base.php
+++ Website/newedituser/base.php
@@ -8,42 +8,40 @@ if(isset($_POST['submit'])){
 function createUser()
 {
 
-/*mysql_connect() or die("MySQL Error: " . mysql_error());
-mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
 
-$con = mysql_connect("192.168.1.128/phpmyadmin","spr_erau","$PRfall2015@ERAU");
+$con = mysqli_connect("192.168.1.128/phpmyadmin","spr_erau","$PRfall2015@ERAU");
 
 if (!$con)
   {
-    die('Could not connect: ' . mysql_error());
+    die('Could not connect: ' . mysqli_error());
   }
 
-mysql_select_db("SE500spr", $con);
+mysqli_select_db("SE500spr", $con);
 
-$First_Name = $_POST['first_name']; //Take "name" from the form
-$Last_Name = $_POST['last_name'];
-$Email = $_POST['email'];
-$Username = $_POST['username'];
-$University = $_POST['university'];
-$Password = $_POST['password'];
+$First_Name = $_POST['FirstName']; //Take "name" from the form
+$Last_Name = $_POST['LastName'];
+$Email = $_POST['Email'];
+$Username = $_POST['Username'];
+//$University = $_POST['university'];
+$Password = $_POST['Password'];
 
+/*
  SQL injection
 
 $users_name = mysql_real_escape_string($users_name);
   $users_email = mysql_real_escape_string($users_email);
   $users_website = mysql_real_escape_string($users_website);
-  $users_comment = mysql_real_escape_string($users_comment);
+  $users_comment = mysql_real_escape_string($users_comment);*/
   
   
   $query = "
   INSERT INTO `SE500`.`Users` (`first_name`, 'last_name', `email`, `username`,
-        `favorited_project`, `uploaded_project`, `university`, `password`) VALUES ('$First_Name',
-        '$Last_Name', '$Email', '$Username', NULL, NULL,
-        $University, '$Password');";
+         `password`) VALUES ('$First_Name',
+        '$Last_Name', '$Email', '$Username', '$Password');";
 		
-	mysql_query($query);
+	mysqli_query($query);
 	
-	mysql_close($con);*/
-	echo "Hello";
+	mysqli_close($con);
+	
 }
 ?>
