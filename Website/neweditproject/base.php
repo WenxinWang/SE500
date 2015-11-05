8b8f935d7a102649ecdc1723f7c3c2eb6028e274
latilm <latilm@my.erau.edu>
2015-11-05 16:26:45 -0500

    Create Project 1.1

---
+++ Website/neweditproject/base.php
@@ -0,0 +1,40 @@
+<?php
+
+if(isset($_POST['submit'])){
+	createProject();
+}
+
+/* session_start(); */
+function createProject()
+{
+
+
+$con = mysqli_connect("localhost","spr_erau","asdf", "SE500spr");
+
+if (!$con)
+  {
+    die('Could not connect: ' . mysqli_connect_error());
+  }
+
+mysqli_select_db("SE500spr", $con);
+
+$Title = $_POST['Title']; 
+$Description = $_POST['Description'];
+  
+  
+  $query = "
+  INSERT INTO Projects (Project_Name, Project_Description) VALUES ('$Title',
+        '$Description')";
+		
+	
+	if (mysqli_query($con, $query)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($con);
+}
+
+	
+	mysqli_close($con);
+	
+}
+?>
