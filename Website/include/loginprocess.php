7680ff415d5c06ba4a61a975c05df18e58d565e4
bellr2 <bellr2@my.erau.edu>
2015-11-09 14:48:55 -0500

    Update and rename Test.txt to loginprocess.php

---
+++ Website/include/loginprocess.php
@@ -0,0 +1,52 @@
+<?php
+    session_start(); // Starting Session
+    $error=''; // Variable To Store Error Message
+    if (isset($_POST['submit']))
+    {
+        if (empty($_POST['username']) || empty($_POST['password'])) 
+        {
+            $error = "Username or Password is missing";
+        }
+        else
+        {
+            // Define $username and $password
+            $username=$_POST['username'];
+            $password=$_POST['password'];
+            
+            // Establishing Connection with Server by passing server_name, user_id and password as a parameter
+            $servername = "localhost";
+            $serverusername = "spr_erau";
+            $serverpassword = "asdf";
+            $dbname = "SE500spr";
+
+            // Create connection
+            $conn = new mysqli($servername, $serverusername, $serverpassword, $dbname);
+            // Check connection
+            if ($conn->connect_error)
+            {
+                die("Connection failed: " . $conn->connect_error);
+            } 
+                    
+            // To protect MySQL injection for Security purpose
+            $username = stripslashes($username);
+            $password = stripslashes($password);
+            //$username = mysql_real_escape_string($username);
+            //$password = mysqli_real_escape_string($password);
+            
+            // SQL query to fetch information of registerd users and finds user match.
+            $sql = "SELECT * FROM login WHERE Username = '" .$username. "' AND BINARY Password = BINARY '" .$password. "'";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) 
+            {
+                $_SESSION['login_user']=$username; // Initializing Session
+                header("location: index.html"); // Redirecting To Other Page
+            }
+            else
+            {
+                $error = "Username or Password is invalid";
+            }
+            $conn->close();
+        }
+    }
+?>
