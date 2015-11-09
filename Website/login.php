0712f2c7624ed3a70a5999dc41169a8e4bbd4936
bellr2 <bellr2@my.erau.edu>
2015-11-09 14:42:04 -0500

    Create login.php

---
+++ Website/login.php
@@ -0,0 +1,15 @@
+<?php
+include('include/SE500loginprocess.php');
+?>
+<html>
+    <form action="" method="post">
+        <label>Username :</label><br/>
+        <input id="name" name="username" type="text"><br/>
+        <label>Password :</label><br/>
+        <input id="password" name="password" type="password"><br/>
+        <br/>
+        <input name="submit" type="submit" class="button" value=" Login ">
+        <br/>
+        <span style="color:red"><?php echo $error; ?></span>
+    </form>
+</html>
