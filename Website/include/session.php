433c0e9aa7e809bb1fde6e9e6dd7e51b15dd1cca
bellr2 <bellr2@my.erau.edu>
2015-11-09 15:13:01 -0500

    Create session.php

---
+++ Website/include/session.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "spr_erau";
+$password = "asdf";
+$dbname = "SE500spr";
+
+// Create connection
+$connection = new mysqli($servername, $username, $password, $dbname);
+// Selecting Database
+session_start();// Starting Session
+// Storing Session
+$user_check=$_SESSION['login_user'];
+// SQL Query To Fetch Complete Information Of User
+$ses_sql=mysqli_query($connection, "SELECT Name FROM login WHERE Username='" .$user_check. "'");
+$row = mysqli_fetch_assoc($ses_sql);
+$login_session =$row['Name'];
+if(!isset($login_session))
+    {
+    $connection->close(); // Closing Connection
+    header('Location: error.html'); // Redirecting To Home Page
+    }
+?>
