c3ff7bb54a472380ccf86d85e0096f6d05db301c
WenxinWang <WANGW4@my.erau.edu>
2015-11-10 08:41:29 -1200

    FINAL

--- Website/results/SearchResults.php
+++ Website/results/SearchResults.php
@@ -45,56 +45,38 @@ mysqli_select_db("SE500spr", $con);
 
 	switch($OrderSearch){
 		case"RL":
-			$search_sql = "SELECT Project_ID, Project_Description,
-				Project_Name, Group_Members, Date_Uploaded, Rating_Total 
-				FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Rating_Total ASC";
+			$search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Rating_Total ASC";
 			break;
 		case"RH":
-			$search_sql = "SELECT Project_ID, Project_Description,
-				Project_Name, Group_Members, Date_Uploaded, Rating_Total
-				FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Rating_Total DESC";
+			$search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Rating_Total DESC";
 			break;
 		case"LH":
-			$search_sql = "SELECT Project_ID, Project_Description,
-				Project_Name, Group_Members, Date_Uploaded, Rating_Total
-				FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Recommended_Grade_Level DESC";
+			$search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total	FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Recommended_Grade_Level DESC";
 			break;
 		case"LL":
-			$search_sql = "SELECT Project_ID, Project_Description,
-				Project_Name, Group_Members, Date_Uploaded, Rating_Total
-				FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Recommended_Grade_Level ASC";
+			$search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Recommended_Grade_Level ASC";
 			break;
 		case"VH":
-			$search_sql = "SELECT Project_ID, Project_Description,
-				Project_Name, Group_Members, Date_Uploaded, Rating_Total
-				FROM $dbName WHERE Project_Name LIKE '%$Search%'";//ORDER BY View_times DESC
+			$search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total	FROM $dbName WHERE Project_Name LIKE '%$Search%'";//ORDER BY View_times DESC
 			break;
 		case"VL":
-			$search_sql = "SELECT Project_ID, Project_Description,
-				Project_Name, Group_Members, Date_Uploaded, Rating_Total
-				FROM $dbName WHERE Project_Name LIKE '%$Search%'";//ORDER BY View_times ASC
+				$search_sql = "SELECT Project_ID, Project_Description,	Project_Name, Group_Members, Date_Uploaded, Rating_Total FROM $dbName WHERE Project_Name LIKE '%$Search%'";//ORDER BY View_times ASC
 			break;
 		case"DH":
-			$search_sql = "SELECT Project_ID, Project_Description,
-				Project_Name, Group_Members, Date_Uploaded, Rating_Total
-				FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Date_Uploaded DESC";
+			$search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Date_Uploaded DESC";
 			break;
 		case"DL":
-			$search_sql = "SELECT Project_ID, Project_Description,
-				Project_Name, Group_Members, Date_Uploaded, Rating_Total
-				FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Date_Uploaded ASC";
+			$search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total	FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Date_Uploaded ASC";
 			break;
 		default:	//(!$OrderSearch)
-			$search_sql = "SELECT Project_ID, Project_Description,
-				Project_Name, Group_Members, Date_Uploaded, Rating_Total
-				FROM $dbName WHERE Project_Name LIKE '%$Search%'";
+			$search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total	FROM $dbName WHERE Project_Name LIKE '%$Search%' desc";
 		}
 		
 		//( Page -1 (i.e. 0 is the first page))
     if(!$searchPage){ // If no search page has been provided - straight from index or advanced search page. 
         $searchPage = 0;
     }
-	$search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Project_ID DESC";
+	// $search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total FROM $dbName WHERE Project_Name LIKE '%$Search%' ORDER BY Project_ID DESC";
 		//here can also return other items of the projects.
 	$search_query = mysqli_query($con, $search_sql);
 	
