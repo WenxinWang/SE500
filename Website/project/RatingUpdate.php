30eafd023ec5cc6e64a14f0bc9258c58bab2973e
Jaiden91 <jaidenkennedy@Jaidens-MacBook-Pro.local>
2015-11-16 15:35:20 -0500

    Added Rating Function

---
+++ Website/project/RatingUpdate.php
@@ -0,0 +1,42 @@
+<?php
+
+
+if(isset($_POST['submit'])){
+	echo "RatingUpdate";
+    $Project=$_GET["ID"];
+	UpdateRating();
+}
+
+/* session_start(); */
+function UpdateRating()
+{
+
+
+$con = mysqli_connect("localhost","spr_erau","asdf", "SE500spr");
+
+if (!$con)
+  {
+    die('Could not connect: ' . mysqli_connect_error());
+  }
+
+mysqli_select_db("SE500spr", $con);
+
+$Value = $_POST['Rating']; 
+
+  
+  
+  $query = "
+  UPDATE Projects SET (Rating_Total = Rating_Total + $Value, Number_of_Ratings = Number_of_Ratings + 1) WHERE Project_ID = $Project";
+		
+	
+	if (mysqli_query($con, $query)) {
+    header("Location:project.php?ID='$Project'");
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($con);
+}
+
+	
+	mysqli_close($con);
+	
+}
+?>
