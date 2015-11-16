9c066158d87303c637d8cb81404547857a038cc5
latilm <latilm@my.erau.edu>
2015-11-16 15:08:05 -0500

    test 2.0

--- Website/newedituser/base.php
+++ Website/newedituser/base.php
@@ -46,8 +46,9 @@ $users_name = mysql_real_escape_string($users_name);
 	//mysqli_query($query);
 	
 	if (mysqli_query($con, $query)) {
+	//echo "New record created successfully";
 	header("Location:../index.html");
-    echo "New record created successfully";
+    //
 } else {
     echo "Error: " . $query . "<br>" . mysqli_error($con);
 }
