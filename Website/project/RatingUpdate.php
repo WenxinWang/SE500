4e189c6cac32cf4afc789fd43426b43a2d587684
bellr2 <bellr2@my.erau.edu>
2015-11-16 16:02:31 -0500

    Update RatingUpdate.php

--- Website/project/RatingUpdate.php
+++ Website/project/RatingUpdate.php
@@ -28,7 +28,7 @@ $Project=$_GET["ID"];
    echo $Project;
   
   $query = "
-  UPDATE Projects SET Rating_Total AS (Rating_Total + $Value) AND Number_of_Ratings AS (Number_of_Ratings + 1) WHERE Project_ID = $Project";
+  UPDATE Projects AS p SET Rating_Total = ($Value + Rating_Total) AND Number_of_Ratings = (1 + Number_of_Ratings) WHERE Project_ID = $Project";
 		
 	
 	if (mysqli_query($con, $query)) {
