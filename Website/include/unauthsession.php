7b0de0e22372428ab9ddb73a2002ff123d5b3aa6
bellr2 <bellr2@my.erau.edu>
2015-11-17 15:00:00 -0500

    Create unauthsession.php

---
+++ Website/include/unauthsession.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "spr_erau";
+$password = "asdf";
+$dbname = "SE500spr";
+// Create connection
+$connection = new mysqli($servername, $username, $password, $dbname);
+// Selecting Database
+session_start();// Starting Session
+// Storing Session
+$user_check=$_SESSION['login_user'];
+// SQL Query To Fetch Complete Information Of User
+$ses_sql=mysqli_query($connection, "SELECT First_Name FROM Users WHERE Username='" .$user_check. "'");
+$row = mysqli_fetch_assoc($ses_sql);
+$login_session =$row['First_Name'];
+?>
