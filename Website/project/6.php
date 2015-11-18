a53984b9f7eeac4c1ca6c1371ecd3f173b0ab7c9
songjiayin28 <254035946@qq.com>
2015-11-18 17:47:17 -0500

    123

--- Website/project/6.php
+++ Website/project/6.php
@@ -29,7 +29,7 @@ else
   echo "Upload: " . $_FILES["file"]["name"] . "<br />";
   echo "Type: " . $_FILES["file"]["type"] . "<br />";
   echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-  echo "Stored in: " . $_FILES["file"]["tmp_name"];
+  echo "Stored in: " . $_FILES["file"]["tmp_name"];<br />
   }
 //展示： 
 	 $image = mysqli_escape_string(file_get_contents($_FILES["file"]["tmp_name"]));    
