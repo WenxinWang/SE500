9d503994ce4d11f92a31d34160813cd7a25498b1
Jaiden91 <jaidenkennedy@Jaidens-MacBook-Pro.local>
2015-11-18 17:49:33 -0500

    Fixed Error In PHP causing runtime failure

--- Website/index.php
+++ Website/index.php
@@ -38,31 +38,32 @@ include('include/unauthsession.php');
         <div class="w-col w-col-2 w-col-small-2 w-col-tiny-4">
           <div class="w-dropdown" data-delay="0">
             <div class="w-dropdown-toggle topmenu">
-                <?php  
-                if(!$LoggedIn){
-                
-             echo '<div class="log-in-button"><span class="in-image-text"><i>Log In</i></span>';
-             echo '</div>';
-             echo '<div class="w-icon-dropdown-toggle usermenu"></div>';
-             echo '</div>';
-             echo '<nav class="w-dropdown-list"><a class="w-dropdown-link" href="newedituser/newedituser.php">New User</a>';
-            </nav>
-              
-                }else{
-                
-              
-            echo '<div class="log-in-button"><span class="in-image-text"><i><?php echo $login_session; ?></i></span>';
+            
+            <?php  
+                if($LoggedIn){
+              echo '<div class="log-in-button"><span class="in-image-text"><i><?php echo $login_session; ?></i></span>';
             echo  '</div>';
             echo  '<div class="w-icon-dropdown-toggle usermenu"></div>';
             echo  '</div>';
             echo  '<nav class="w-dropdown-list"><a class="w-dropdown-link" href="user/user.php?ID=';
             echo  $UserId;
             echo  '">Profile</a><a class="w-dropdown-link" >Log Out</a><a class="w-dropdown-link" href="neweditproject/neweditproject.php">New Project</a>';
-            echo '</nav>';
+            echo '</nav>';    
+            
+              
+                }else{
+                
+               echo '<div class="log-in-button"><span class="in-image-text"><i>Log In</i></span>';
+             echo '</div>';
+             echo '<div class="w-icon-dropdown-toggle usermenu"></div>';
+             echo '</div>';
+             echo '<nav class="w-dropdown-list"><a class="w-dropdown-link" href="newedituser/newedituser.php">New User</a>';
+             echo '</nav>';
+           
               
               
                 }
-                ?>
+             ?>
           </div>
         </div>
       </div>
