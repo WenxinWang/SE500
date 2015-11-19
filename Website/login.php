ad3db5d5c4effa89aef37bbc7f8c4ebb68b8c90e
Jaiden91 <jaidenkennedy@Jaidens-MacBook-Pro.local>
2015-11-18 19:26:25 -0500

    undid change

--- Website/login.php
+++ Website/login.php
@@ -1,76 +1,9 @@
 <?php
-include ('include/unauthsession.php')
+
 include('include/loginprocess.php');
 ?>
 <html>
-    
-    <head>
-  <meta charset="utf-8">
-  <title>Project Hunter GUI</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="generator" content="Webflow">
-  <link rel="stylesheet" type="text/css" href="css/normalize.css">
-  <link rel="stylesheet" type="text/css" href="css/webflow.css">
-  <link rel="stylesheet" type="text/css" href="css/jaidens-supercool-site.webflow.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
-  <script>
-    WebFont.load({
-      google: {
-        families: ["Exo:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"]
-      }
-    });
-  </script>
-  <script type="text/javascript" src="js/modernizr.js"></script>
-  <link rel="shortcut icon" type="image/x-icon" href="https://daks2k3a4ib2z.cloudfront.net/img/favicon.ico">
-  <link rel="apple-touch-icon" href="https://daks2k3a4ib2z.cloudfront.net/img/webclip.png">
-</head>
-<body>
-  <div class="w-section head-panel">
-    <div class="w-container top-menu"></div>
-  </div>
-  <div class="w-section toprowsection">
-    <div class="w-container toprowcontainer">
-      <div class="w-row toprow">
-        <div class="w-col w-col-8 w-col-small-8 w-col-tiny-4"></div>
-        <div class="w-col w-col-2 w-col-small-2 w-col-tiny-4 about-column"><a class="about" href="about/about.php">About</a>
-        </div>
-        <div class="w-col w-col-2 w-col-small-2 w-col-tiny-4">
-          <div class="w-dropdown" data-delay="0">
-            <div class="w-dropdown-toggle topmenu">
-            
-            <?php  
-                if($LoggedIn){
-              echo '<div class="log-in-button"><span class="in-image-text" ><i>';
-                echo $login_session;
-                  echo  '</i></span>';
-            echo  '</div>';
-            echo  '<div class="w-icon-dropdown-toggle usermenu"></div>';
-            echo  '</div>';
-            echo  '<nav class="w-dropdown-list"><a class="w-dropdown-link" href="user/user.php?ID=';
-            echo  $UserId;
-            echo  '">Profile</a><a class="w-dropdown-link" href="logouttest.php">Log Out</a><a class="w-dropdown-link" href="neweditproject/newproject.php">New Project</a>';
-            echo '</nav>';    
-            
-              
-                }else{
-                
-               echo '<div class="log-in-button"><span class="in-image-text"><i>Log In</i></span>';
-             echo '</div>';
-             echo '<div class="w-icon-dropdown-toggle usermenu"></div>';
-             echo '</div>';
-             echo '<nav class="w-dropdown-list"><a class="w-dropdown-link" href="login.php">Log In</a>';
-             echo '<a class="w-dropdown-link" href="newedituser/newedituser.php">New User</a>';
-             echo '</nav>';
-           
-              
-              
-                }
-             ?>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+
     
     <form action="" method="post">
         <label>Username :</label><br/>
