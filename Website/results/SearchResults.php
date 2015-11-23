2df335a4f3615f385a1241b959032f5bbf9aa600
WenxinWang <WANGW4@my.erau.edu>
2015-11-23 17:45:56 -0500

    ID instead of keyword for advanced search

--- Website/results/SearchResults.php
+++ Website/results/SearchResults.php
@@ -59,7 +59,7 @@ Primary_Programming_Language	//Language
 
 //---------------------------------------------------------------------------------------------------------------
 */
-$Advanced_Search_Keyword=$_GET["Keyword"];
+
 $Advanced_Search_ID=$_GET["ID"];
 $Advanced_Search_Begin_UploadDateRange=$_GET["Begin_UploadDateRange"];
 $Advanced_Search_End_UploadDateRange=$_GET["End_UploadDateRange"];
@@ -72,8 +72,8 @@ $Advanced_Search_Authors=$_GET["Authors"];
 $Advanced_Search_Language=$_GET["Language"];
 
 if ($OrderSearch){
-	if($Advanced_Search_Keyword){
-    $search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total	FROM $dbName WHERE /*Project_Name Like '%$Advanced_Search_Keyword%' OR */Project_ID = '%$Advanced_Search_ID%' OR Date_Uploaded >= $Advanced_Search_Begin_UploadDateRange OR Date_Last_Updated <= $Advanced_Search_End_UploadDateRange OR University LIKE '%$Advanced_Search_University%' OR Rating_Total LIKE '%$Advanced_Search_Rating%' OR Recommended_Grade_Level LIKE '%$Advanced_Search_Level%' OR Completion_Status LIKE '%$Advanced_Search_Status%' OR Group_Members LIKE '%$Advanced_Search_Authors%' OR Primary_Programming_Language LIKE '%$Advanced_Search_Language%'";
+	if($Advanced_Search_ID){
+    $search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total	FROM $dbName WHERE Project_ID = '%$Advanced_Search_ID%' OR Date_Uploaded >= $Advanced_Search_Begin_UploadDateRange OR Date_Last_Updated <= $Advanced_Search_End_UploadDateRange OR University LIKE '%$Advanced_Search_University%' OR Rating_Total LIKE '%$Advanced_Search_Rating%' OR Recommended_Grade_Level LIKE '%$Advanced_Search_Level%' OR Completion_Status LIKE '%$Advanced_Search_Status%' OR Group_Members LIKE '%$Advanced_Search_Authors%' OR Primary_Programming_Language LIKE '%$Advanced_Search_Language%'";
 	}else{
 		$search_sql = "SELECT Project_ID, Project_Description, Project_Name, Group_Members, Date_Uploaded, Rating_Total	FROM $dbName WHERE Project_Name LIKE '%$Search%'";
 	}
